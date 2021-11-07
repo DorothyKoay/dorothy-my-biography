ff0df557474208b10fb6e1791ae1b92065e9fa19
DorothyKoay <koayy35@gmail.com>
2021-11-07 06:11:55 +0000

    commit

---
+++ src/lib/Section.svelte
@@ -0,0 +1,13 @@
+<script>
+    export let title;
+    export let item1;
+    export let item2;
+    export let item3;
+
+</script>
+<div class="card-body">
+    <h3>{title}</h3>
+    <p>{item1}</p>
+    <p>{item2}</p>
+    <p>{item3}</p>
+</div>
--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -1,2 +1,31 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+<script>
+	import Header from '$lib/Header.svelte';
+	import Section from '$lib/Section.svelte';
+	import Footer from '$lib/Footer.svelte';
+	let myInfo = {
+		title: 'Info',
+		item1: 'Koay Yi Wen Dorothy',
+		item2: 12,
+		item3: 'SJK(C) Perempuan China'
+	};
+	let myHobby = {
+		title: 'Hobby',
+		item1: 'Playing Piano',
+		item2: 'Cooking',
+		item3: 'Play badminton'
+	};
+	let myPersonality = {
+		title: 'Personality',
+		item1: 'Quiet',
+		item2: 'Humble',
+		item3: 'Kind'
+	};
+</script>
+
+<div>
+	<Header title="My Biography" />
+	<Section {...myInfo} />
+	<Section {...myHobby} />
+	<Section {...myPersonality} />
+	<Footer gitHub="https://github.com/DorothyKoay/dorothy-my-biography" />
+</div>
